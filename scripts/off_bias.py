cfb57048e8b4c4055642abf0aa038a1e420ed5ab
AlexRed806 <dr.alessandro.minotti@gmail.com>
2024-03-16 20:05:29 +0100

    Changed index for iteration in off_bias.py

--- scripts/off_bias.py
+++ scripts/off_bias.py
@@ -1,8 +1,8 @@
 import ivtools
 
 def main():
-    for i in [4,5,7,9,11,12,13]:
-        ip=f'10.73.137.{100+i}'
+    for j in [4,5,7,9,11,12,13]:
+        ip=f'10.73.137.{100+j}'
         disable_bias=ivtools.Command(ip, f'WR VBIASCTRL V {0}')
         set_bias=[ivtools.Command(ip, f'WR BIASSET AFE {i} V {0}') for i in range (5)] 
         apply_trim=[ivtools.Command(ip, f'WR TRIM CH {i} V {0}')for i in range (40)]
