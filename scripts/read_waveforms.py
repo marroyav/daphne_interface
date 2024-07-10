2daea389652489e3b1a47995ae32880841c3b0d0
manuel <matheos.mc2@gmail.com>
2024-07-10 21:21:48 +0200

    read_waveforms

---
+++ scripts/read_waveforms.py
@@ -0,0 +1,44 @@
+import ivtools, click
+
+@click.command()
+@click.option("--ip_address", '-ip', default='ALL',help="IP Address")
+def main(ip_address):
+    '''
+    This script checks that the data output in DAPHNE is OK.
+    The expected output are chucks of data with the structure of the frames configured in data modes.
+
+    Args: 
+        - ip_address (default='ALL'): if no argument given it runs over all endpoints.
+    
+    Example: python conf_analog.py (-ip 4,5)
+    '''
+    
+    print(f"\033[35mExpecting: Different waveforms\033[0m")
+
+    if ip_address=="ALL": your_ips = [4,5,7,9,11,12,13]
+    else: your_ips = your_ips = list(map(int, list(ip_address.split(","))))
+
+    for ip in your_ips:
+        if ip not in [4,5,7,9,11,12,13]: 
+            print("\033[91mInvalid IP address, please choose your ip between 40 :)\033[0m"); 
+            exit()
+        interface = ivtools.daphne(f"10.73.137.{100+ip}")
+        interface.write_reg(0x2000, [1234])
+        rec=[]
+        print(f"\033[91m\nChecking ip address 192.168.121.{ip} data out in DAPHNE:\033[0m")
+        print()
+        for i in range (10):
+            doutrec = interface.read_reg(0x40600000+i*128,128)
+            for word in doutrec[2:]:
+                rec.append(word)
+        #  for word in doutrec[3:]:
+        # print (hex(rec[0]))
+        for i in range (len(rec)):
+            if rec[i] == 0x000000BC:
+                print("%08X" % rec[i])
+            else:
+                print("%08X" % rec[i],end=' ')
+    interface.close()
+
+if __name__ == "__main__":
+    main()
