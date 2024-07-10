4ccd341f2013acc208addf6b3a29bb0170a9f424
manuel <matheos.mc2@gmail.com>
2024-07-10 23:21:33 +0200

    colorcitos

--- scripts/configuration/check_counters.py
+++ scripts/configuration/check_counters.py
@@ -2,25 +2,34 @@ import ivtools, click
 
 @click.command()
 @click.option("--ip_address", '-ip', default='ALL',help="IP Address")
+
+
 def main(ip_address):
     '''
-    This script checks the datamode for each endpoint configured with conf_datamodes.py.
+    This script spy on the counters for each endpoint.
     The expected output is:
-        DAPHNE physical scheme
-        ADDRESS         SLOT    REG     MODE
-        10.73.137.104   4       0xaa    full streaming
-        10.73.137.105   5       0xaa    full streaming
-        10.73.137.107   7       0xaa    full streaming
-        10.73.137.109   9       0x3     self trigger
-        10.73.137.111   11      0x3     self trigger
-        10.73.137.112   12      0x3     self trigger
-        10.73.137.113   13      0x3     self trigger
+        Checking Counters
+        ADDRESS 10.73.137.112
+        TRIGGER         FIFO            FLX
+        0000000039      0000000015      0000001097
+        0000000047      0000000014      0000001097
+        0000000019      0000000015      0000001097
+        0000000004      0000000014      0000001097
+        0000000014      0000000016      0000001097
+        0000000042      0000000016      0000001097
         
     Args: 
         - ip_address (default='ALL'): if no argument given it runs over all endpoints.
     
     Example: python conf_analog.py (-ip 4,5)
     '''
+    RED = "\033[31m"
+    GREEN = "\033[32m"
+    YELLOW = "\033[33m"
+    BLUE = "\033[34m"
+    MAGENTA = "\033[35m"
+    CYAN = "\033[36m"
+    RESET = "\033[0m"
 
     if ip_address=="ALL": your_ips = [4,5,7,9,11,12,13]
     else: your_ips = your_ips = list(map(int, list(ip_address.split(","))))
@@ -31,17 +40,17 @@ def main(ip_address):
             exit()
         interface = ivtools.daphne(f"10.73.137.{100+ip}")
         interface.write_reg(0x2001, [1234]) # software trigger, all spy buffers capture
-        print ("Checking Counters")
-        print ("ADDRESS",end='\t')
-        print (f"10.73.137.{100+ip}")
-        print ("TRIGGER", end='\t\t\t')
-        print ("FIFO", end='\t\t')
-        print ("FLX")
+        print (f"{BLUE}Checking Counters{RESET}")
+        print (f"{BLUE}ADDRESS{RESET}",end='\t')
+        print (f"{RED}10.73.137.{100+ip}{RESET}")
+        print (f"{CYAN}TRIGGER{RESET}", end='\t\t')
+        print (f"{CYAN}FIFO{RESET}", end='\t\t')
+        print (f"{CYAN}FLX{RESET}")
         trigger = [(interface.read_reg(0x40800000+n*0x8,1)[2]) for n in range(40)  ]
         fifo = [(interface.read_reg(0x40800140+n*0x8,1)[2]) for n in range(40)]
         flx = (interface.read_reg(0x40800280,1)[2])
         for i in range (40):
-            print(f"{trigger[i]}",end='\t'),print(f"{fifo[i]}",end='\t'),print(f"{flx}",end='\t'),print()
+            print(f"{GREEN}{trigger[i]:010}",end='\t'),print(f"{GREEN}{fifo[i]:010}",end='\t'),print(f"{YELLOW}{flx:010}{RESET}",end='\t'),print()
         
         interface.close()
 
